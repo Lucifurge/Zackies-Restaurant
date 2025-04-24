0a6ed558bfd51e0e88501ecf1b3e7cf03944b24a
Lucifurge <tasogaresaya@gmail.com>
2025-04-24 15:53:22 +0800

    Update nav.js

--- nav.js
+++ nav.js
@@ -1,8 +1,12 @@
-document.querySelectorAll('.navbar-nav .nav-link').forEach(link => {
-  link.addEventListener('click', () => {
-    const navbarCollapse = document.querySelector('.navbar-collapse');
+document.addEventListener('DOMContentLoaded', function () {
+  const navbarToggler = document.querySelector('.navbar-toggler');
+  const navbarCollapse = document.querySelector('.navbar-collapse');
+
+  navbarToggler.addEventListener('click', function () {
     if (navbarCollapse.classList.contains('show')) {
-      new bootstrap.Collapse(navbarCollapse).hide();
+      navbarCollapse.classList.remove('show');
+    } else {
+      navbarCollapse.classList.add('show');
     }
   });
 });
