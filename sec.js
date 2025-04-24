9067c14ee99d6db9e3e1b0d49180e4b81238591b
Lucifurge <tasogaresaya@gmail.com>
2025-04-24 21:19:05 +0800

    Create sec.js

---
+++ sec.js
@@ -0,0 +1,22 @@
+<script>
+  document.addEventListener("DOMContentLoaded", () => {
+    const sections = document.querySelectorAll(".animated-section");
+
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            entry.target.classList.add("in-view");
+          }
+        });
+      },
+      {
+        threshold: 0.2,
+      }
+    );
+
+    sections.forEach((section) => {
+      observer.observe(section);
+    });
+  });
+</script>
